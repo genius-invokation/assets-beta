c385b3ef947bbfe1b6432ef5b541ec9438356179
Guyutongxue <guyutongxue@163.com>
2024-12-30 16:48:12 +0800

    redirect character_icons to parent api

---
+++ api/v2/images/character_icons/[id].js
@@ -0,0 +1,18 @@
+// @ts-check
+
+/**
+ * @typedef {import("@vercel/node").VercelRequest} VercelRequest
+ * @typedef {import("@vercel/node").VercelResponse} VercelResponse
+ */
+
+import parentHandler from "../[id].js";
+
+/**
+ *
+ * @param {VercelRequest} req
+ * @param {VercelResponse} res
+ * @returns
+ */
+export default function handler(req, res) {
+  return parentHandler(req, res);
+}
