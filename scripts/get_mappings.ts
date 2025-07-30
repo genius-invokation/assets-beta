3cff410dd9a689982aa24be6e7b2619c3ca79b4d
Guyutongxue <guyutongxue@163.com>
2025-07-30 22:02:19 +0800

    adjust

--- scripts/get_mappings.ts
+++ scripts/get_mappings.ts
@@ -4,21 +4,20 @@ import entities from "../data/entities.json";
 const skills = [...characters, ...entities].flatMap((obj: any) => obj.skills);
 
 export const ICON_MAPPINGS = {
-  // 5.8
-  15122: "Skill_S_Lanyan_01",
-  15123: "Skill_E_Lanyan_01_HD",
-  115121: "UI_Gcg_Buff_Lanyan_E1",
-  15132: "Skill_S_Heizo_01",
-  15133: "Skill_E_Heizo_01_HD",
-  15134: "UI_Talent_S_Heizo_05",
-  115132: "UI_Gcg_Buff_Heizo_E1",
-  115133: "UI_Gcg_DeBuff_Heizo_S",
-  115134: "UI_Gcg_DeBuff_Heizo_S",
-  115135: "UI_Gcg_DeBuff_Heizo_S",
-  115136: "UI_Gcg_DeBuff_Heizo_S",
-  27042: "MonsterSkill_S_HookwalkerPrimo_01",
-  27043: "MonsterSkill_E_HookwalkerPrimo_01_HD",
-  27044: "MonsterSkill_S_HookwalkerPrimo_02",
+  // 6.0
+  14142:"Skill_S_Iansan_01",  
+  14143:"Skill_E_Iansan_01_HD",
+  14144:"UI_Talent_S_Iansan_08",
+  15142:"Skill_S_Mizuki_01",
+  15143:"Skill_E_Mizuki_01_HD",
+  23052:"MonsterSkill_S_TheAbyssXiuhcoatl_01",
+  23053:"MonsterSkill_E_TheAbyssXiuhcoatl_01_HD",
+  23054:"MonsterSkill_S_TheAbyssXiuhcoatl_02",
+
+  114141:"UI_Gcg_Buff_Nightsoul_Elec",
+  114142:"UI_Gcg_Buff_Iansan_S",
+  115141:"UI_Gcg_Buff_Mizuki_S",
+  123051:"UI_Gcg_Buff_TheAbyssXiuhcoatl_S",
 };
 
 const skillIconMappings: Record<string, string> = {};
